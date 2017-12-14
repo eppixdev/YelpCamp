15bc7075f444b2241997d1b0de462d4198312461
eppixdev <hatkoff@gmail.com>
2017-12-13 19:49:10 -0800

    Added POST route for adding new campgrounds - Added bootstrap

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 var express     = require('express'),
+    bodyParser  = require('body-parser'),
     app         = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+ var campgrounds = [ 
+        {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1274/4670974422_ec49d65ab2.jpg"},
+        {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
+        {name: "Mountain Camp", image: "https://farm5.staticflickr.com/4076/4819277877_109e312af2.jpg"}
+    ]
 
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-    var campgrounds = [ 
-        {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1274/4670974422_ec49d65ab2.jpg"},
-        {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-        {name: "Mountain Camp", image: "https://farm5.staticflickr.com/4076/4819277877_109e312af2.jpg"}
-    ]
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', function(req, res) {
+    // get data from form and add to campground array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    // redirect back to campgrounds page
+    res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new');
 });
 
 // listen for server start
