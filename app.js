d975970352ca7e999c519a8bb84385636afd2c15
Chris Hatkoff <hatkoff@gmail.com>
2017-12-18 03:19:16 +0000

    npm installed mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express     = require('express'),
     bodyParser  = require('body-parser'),
-    app         = express();
+    app         = express(),
+    mongoose    = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
