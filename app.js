48741b7a3484953fc0ea1db2062183c659f9c607
Chris Hatkoff <hatkoff@gmail.com>
2017-12-18 03:16:01 +0000

    Eliminated some campground test data

--- app.js
+++ app.js
@@ -8,20 +8,13 @@ app.set('view engine', 'ejs');
  var campgrounds = [ 
         {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1274/4670974422_ec49d65ab2.jpg"},
         {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-         {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-         {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-         {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-         {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-         {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-         {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
-        {name: "Mountain Camp", image: "https://farm5.staticflickr.com/4076/4819277877_109e312af2.jpg"}
+        {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"},
+        {name: "Granite Hill", image: "https://farm6.staticflickr.com/5328/8808095836_5e61be09a6.jpg"}
 ]
 
 app.get('/', function(req, res) {
     res.render('landing');
 });
-
-console.log('test');
 
 app.get('/campgrounds', function(req, res) {
     res.render('campgrounds', {campgrounds: campgrounds});
